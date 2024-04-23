306aac6a565e1b76f4286ca8e052dd95ba084cff
Raymond Luong <raymond@mercury.com>
2024-04-22 17:10:47 -0700

    Update tooling page with sections

--- src/components/tooling/tooling-page-content.tsx
+++ src/components/tooling/tooling-page-content.tsx
@@ -1,74 +1,179 @@
 import { useState } from 'react';
 import { Input } from '../ui/input';
 import { Badge } from '../ui/badge';
-import { tools as toolsData } from './tools';
+import {
+  frameworks,
+  buildTools,
+  lintersAndFormatters,
+  testing,
+  styling,
+  runtimes,
+  packageManagers,
+  designSystems,
+} from './tools';
+import { Button } from '../ui/button';
+
+const categories = [
+  'Framework',
+  'Build Tool',
+  'Design System',
+  'Testing',
+  'Styling',
+  'Linter/formatter',
+  'Package Manager',
+  'Runtime',
+] as const;
+
+const categoryTitles = {
+  'Build Tool': 'Build Tools',
+  'Design System': 'Design Systems',
+  Framework: 'Frameworks',
+  'Linter/formatter': 'Linters & Formatters',
+  'Package Manager': 'Package Managers',
+  Runtime: 'Runtimes',
+  Testing: 'Testing',
+  Styling: 'Styling',
+};
+
+const categoryDescriptions = {
+  Framework:
+    'Frameworks offer structured environments for building web applications, equipped with pre-built components and tools to simplify development. The right framework for a project depends on the specific needs of the application, such as interactivity level, content volume, and performance requirements. For interactive applications, choose a framework with robust features and a strong ecosystem. For simpler, static sites, a lighter framework that optimizes for quick loading and ease of use may be more suitable. When selecting a framework, consider factors like project requirements, developer experience, and community support to ensure efficient development and maintenance.',
+  'Build Tool':
+    'Build tools automate the process of turning source code into production-ready scripts and stylesheets by handling tasks such as bundling, minification, and transpilation. They help optimize development workflows and ensure that applications perform efficiently on the web.',
+  'Design System':
+    'Design systems consist of reusable components and standards for design and code. These systems enable teams to build consistent, scalable, and high-quality user interfaces across products.',
+  Testing:
+    'Testing tools help ensure the quality and functionality of web applications by automating the testing process. These tools can simulate user interactions, check code for errors, and verify that components behave as expected under various conditions.',
+  Styling:
+    'Styling tools assist developers in designing and managing the look and feel of web applications. They range from CSS frameworks that provide ready-to-use stylesheets to preprocessors and libraries that offer advanced features like variables and mixins.',
+  'Linter/formatter':
+    'Linters and formatters are tools that analyze code for potential errors, style inconsistencies, and enforce coding standards. They help maintain code quality, enhance readability, and reduce bugs in development.',
+  'Package Manager':
+    'Package managers simplify the process of managing project dependencies and libraries. They automate the installation, upgrading, and configuration of packages, making it easy to manage complex projects.',
+  Runtime:
+    'Runtimes are environments where JavaScript code executes. They provide the necessary infrastructure to run JavaScript code outside of browsers, such as on servers or in other computing environments.',
+};
+
+const categoryItems = {
+  Framework: frameworks,
+  'Build Tool': buildTools,
+  'Design System': designSystems,
+  Testing: testing,
+  Styling: styling,
+  Runtime: runtimes,
+  'Package Manager': packageManagers,
+  'Linter/formatter': lintersAndFormatters,
+};
+
+type Category = (typeof categories)[number];
 
 type Tool = {
   name: string;
   description: string;
   url: string;
-  tags: ToolTag[];
+  category: Category;
+  tags?: string[];
   authorFavorite?: boolean;
 };
 
-const toolTagValues = [
-  'Bundler',
-  'Compiler',
-  'CSS',
-  'Design System',
-  'Formatter',
-  'Framework',
-  'Headless UI',
-  'Language',
-  'Linter',
-  'Package Manager',
-  'Runtime',
-  'Rust',
-  'Testing',
-] as const;
-
-export type ToolTag = (typeof toolTagValues)[number];
-
-const tools = toolsData as Tool[];
-const sortedTools = tools.sort((a, b) => {
-  if (a.authorFavorite && !b.authorFavorite) {
-    return -1;
-  }
-  if (!a.authorFavorite && b.authorFavorite) {
-    return 1;
-  }
-  return a.name.localeCompare(b.name);
-});
+const tools = [
+  ...frameworks,
+  ...buildTools,
+  ...lintersAndFormatters,
+  ...testing,
+  ...styling,
+  ...runtimes,
+  ...packageManagers,
+  ...designSystems,
+] as Tool[];
 
 export const ToolingPageContent = () => {
   const [search, setSearch] = useState('');
-  const filteredTools = sortedTools.filter((tool) =>
+  const filteredTools = tools.filter((tool) =>
     tool.name.toLowerCase().includes(search.toLowerCase()),
   );
+  const isSearching = search.length > 0;
   return (
-    <div className="space-y-5 mt-5">
-      <div className="flex items-center gap-5">
+    <div className="mt-5">
+      <div className="flex flex-col gap-5">
         <Input
           placeholder="Search by name"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           className="w-full sm:w-1/2"
         />
+        <div className="flex gap-2 items-center flex-wrap">
+          <span className="text-sm text-muted-foreground">Jump to:</span>
+          {categories.map((category) => (
+            <Button
+              key={category}
+              onClick={() => {
+                document.getElementById(category)?.scrollIntoView({
+                  behavior: 'smooth',
+                });
+              }}
+              variant="secondary"
+              size="sm"
+            >
+              {categoryTitles[category]}
+            </Button>
+          ))}
+        </div>
       </div>
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-        {filteredTools.map((tool) => (
-          <ToolingCard key={tool.name} tool={tool} />
-        ))}
-      </div>
+      {isSearching ? (
+        <>
+          <p className="text-sm text-muted-foreground my-5">
+            Showing {filteredTools.length}{' '}
+            {filteredTools.length === 1 ? 'tool' : 'tools'} matching your search
+          </p>
+          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+            {filteredTools.map((tool) => (
+              <ToolingCard key={tool.name} tool={tool} showCategory />
+            ))}
+          </div>
+        </>
+      ) : (
+        categories.map((category) => {
+          const title = categoryTitles[category];
+          const items = categoryItems[category] as Tool[];
+          items.sort((a, b) => {
+            // Put author favorite first then sort by name
+            if (a.authorFavorite && !b.authorFavorite) return -1;
+            if (!a.authorFavorite && b.authorFavorite) return 1;
+            return a.name.localeCompare(b.name);
+          });
+          return (
+            <section
+              key={category}
+              id={category}
+              className="space-y-5 my-10 sm:scroll-mt-20"
+            >
+              <h2 className="text-3xl font-bold">{title}</h2>
+              <p className="text-muted-foreground">
+                {categoryDescriptions[category]}
+              </p>
+              <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+                {items.map((tool) => (
+                  <ToolingCard
+                    key={tool.name}
+                    tool={tool}
+                  />
+                ))}
+              </div>
+            </section>
+          );
+        })
+      )}
     </div>
   );
 };
 
 type ToolingCardProps = {
   tool: Tool;
+  showCategory?: boolean;
 };
-const ToolingCard = (props: ToolingCardProps) => {
-  const { name, description, url, tags, authorFavorite } = props.tool;
+const ToolingCard = ({ tool, showCategory }: ToolingCardProps) => {
+  const { name, description, url, category, authorFavorite } = tool;
   return (
     <a
       href={url}
@@ -84,16 +189,11 @@ const ToolingCard = (props: ToolingCardProps) => {
           )}
         </div>
         <div className="flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <Badge
-              key={tag}
-              radius="md"
-              variant="default"
-              className="capitalize"
-            >
-              {tag}
+          {showCategory && (
+            <Badge radius="md" variant="default" className="capitalize">
+              {category}
             </Badge>
-          ))}
+          )}
           {authorFavorite && (
             <Badge radius="md" variant="outline">
               ⭐ Author Favorite
--- src/components/tooling/tools.ts
+++ src/components/tooling/tools.ts
@@ -1,38 +1,70 @@
-export const tools = [
+export const frameworks = [
   {
     name: 'React',
     description:
       'React is a free and open-source front-end JavaScript library for building user interfaces based on components.',
     url: 'https://reactjs.org/',
-    tags: ['Framework'],
+    category: 'Framework',
   },
   {
     name: 'Vue',
     description:
       ' An approachable, performant and versatile framework for building web user interfaces.',
     url: 'https://vuejs.org/',
-    tags: ['Framework'],
+    category: 'Framework',
   },
   {
     name: 'Angular',
     description:
       'Angular is a development platform for building mobile and desktop web applications using TypeScript/JavaScript and other languages.',
     url: 'https://angular.io/',
-    tags: ['Framework'],
+    category: 'Framework',
   },
   {
     name: 'Svelte',
     description:
       "Svelte is a new way to build web applications. It's a compiler that takes your declarative components and converts them into efficient JavaScript that surgically updates the DOM.",
     url: 'https://svelte.dev/',
-    tags: ['Framework'],
+    category: 'Framework',
   },
+  {
+    name: 'Astro',
+    description:
+      "The web framework for content-driven websites. Astro powers the world's fastest websites, client-side web apps, dynamic API endpoints, and everything in-between.",
+    url: 'https://astro.build/',
+    category: 'Framework',
+    authorFavorite: true,
+  },
+  {
+    name: 'Solid',
+    description:
+      'Solid is a declarative JavaScript library for creating user interfaces. Instead of using a Virtual DOM, it compiles its templates to real DOM nodes and updates them with fine-grained reactions.',
+    url: 'https://www.solidjs.com/',
+    category: 'Framework',
+  },
+  {
+    name: 'Next',
+    description:
+      "Used by some of the world's largest companies, Next.js enables you to create high-quality web applications with the power of React components.",
+    url: 'https://nextjs.org/',
+    category: 'Framework',
+  },
+  {
+    name: 'Nuxt',
+    description:
+      "The Intuitive Vue Framework. Nuxt is an open source framework that makes web development intuitive and powerful. Create performant and production-grade full-stack web apps and websites with confidence.",
+    url: 'https://nuxt.com/',
+    category: 'Framework',
+  },
+];
+
+export const buildTools = [
   {
     name: 'Vite',
     description:
       'A build tool that aims to provide a faster and leaner development experience for modern web projects.',
     url: 'https://vitejs.dev/',
-    tags: ['Bundler'],
+    category: 'Build Tool',
     authorFavorite: true,
   },
   {
@@ -40,110 +72,133 @@ export const tools = [
     description:
       'Webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset.',
     url: 'https://webpack.js.org/',
-    tags: ['Bundler'],
+    category: 'Build Tool',
   },
   {
     name: 'Rollup',
     description:
       'Rollup is a module bundler for JavaScript which compiles small pieces of code into something larger and more complex, such as a library or application.',
     url: 'https://rollupjs.org/',
-    tags: ['Bundler'],
+    category: 'Build Tool',
+  },
+  {
+    name: 'Rolldown (in development)',
+    description:
+      'Rolldown is a JavaScript bundler written in Rust intended to serve as the future bundler used in Vite. It provides Rollup-compatible APIs and plugin interface, but will be more similar to esbuild in scope.',
+    url: 'https://rolldown.rs/',
+    category: 'Build Tool',
   },
   {
     name: 'Snowpack',
     description:
       "Snowpack is a lightning-fast frontend build tool, designed to leverage JavaScript's native module system (known as ESM).",
     url: 'https://www.snowpack.dev/',
-    tags: ['Bundler'],
+    category: 'Build Tool',
   },
   {
     name: 'Turbopack',
     description:
       'Turbopack is an incremental bundler optimized for JavaScript and TypeScript, written in Rust.',
     url: 'https://turbo.build/pack',
-    tags: ['Bundler', 'Rust'],
+    category: 'Build Tool',
+    tags: ['Rust'],
   },
   {
     name: 'ESBuild',
     description: 'An extremely fast bundler for the web',
     url: 'https://esbuild.github.io/',
-    tags: ['Bundler'],
-  },
-  {
-    name: 'TypeScript',
-    description:
-      'TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.',
-    url: 'https://www.typescriptlang.org/',
-    tags: ['Language'],
+    category: 'Build Tool',
   },
   {
     name: 'Babel',
     description:
       "Babel is a tool that helps you write code in the latest version of JavaScript. When your supported environments don't support certain features natively, Babel will help you compile those features down to a supported version.",
     url: 'https://babeljs.io/',
-    tags: ['Compiler'],
+    category: 'Build Tool',
   },
   {
     name: 'SWC',
     description:
       'SWC is an extensible Rust-based platform for the next generation of fast developer tools. SWC can be used for both compilation and bundling.',
     url: 'https://swc.rs/',
-    tags: ['Compiler', 'Rust'],
+    category: 'Build Tool',
+    tags: ['Rust'],
   },
+  {
+    name: 'Parcel',
+    description:
+      'Parcel is a zero configuration build tool for the web. It combines a great out-of-the-box development experience with a scalable architecture that can take your project from just getting started to massive production application.',
+    url: 'https://parceljs.org/',
+    category: 'Build Tool',
+  }
+];
+
+export const lintersAndFormatters = [
   {
     name: 'ESLint',
     description:
       'ESLint statically analyzes your code to quickly find problems. It is built into most text editors and you can run ESLint as part of your continuous integration pipeline.',
     url: 'https://eslint.org/',
-    tags: ['Linter'],
+    category: 'Linter/formatter',
     authorFavorite: true,
   },
   {
     name: 'Prettier',
-    description: 'An opinionated code formatter',
+    description: 'Prettier is an opinionated code formatter. It enforces a consistent style by parsing your code and re-printing it with its own rules that take the maximum line length into account, wrapping code when necessary.',
     url: 'https://prettier.io/',
-    tags: ['Formatter'],
+    category: 'Linter/formatter',
   },
   {
     name: 'TypeScript ESLint',
     description:
-      'The tooling that enables ESLint and Prettier to support TypeScript',
+      'The tooling that enables ESLint and Prettier to support TypeScript.',
     url: 'https://typescript-eslint.io/',
-    tags: ['Linter', 'Formatter'],
+    category: 'Linter/formatter',
+  },
+  {
+    name: 'Stylelint',
+    description:
+      'A mighty CSS linter that helps you avoid errors and enforce conventions.',
+    url: 'https://stylelint.io/',
+    category: 'Linter/formatter',
   },
   {
     name: 'Biome',
     description:
       'A toolchain for web projects, aimed to provide functionalities to maintain them. Biome offers formatter and linter, usable via CLI and LSP.',
     url: 'https://biomejs.dev/',
-    tags: ['Linter', 'Formatter', 'Rust'],
+    category: 'Linter/formatter',
+    tags: ['Rust'],
   },
+];
+
+export const testing = [
   {
     name: 'Jest',
     description:
       'Jest is a delightful JavaScript Testing Framework with a focus on simplicity.',
     url: 'https://jestjs.io/',
-    tags: ['Testing'],
+    category: 'Testing',
   },
   {
     name: 'Cypress',
     description:
       'With Cypress, you can easily create tests for your modern web applications, debug them visually, and automatically run them in your continuous integration builds.',
     url: 'https://www.cypress.io/',
-    tags: ['Testing'],
+    category: 'Testing',
   },
   {
     name: 'Vitest',
     description: 'Next generation testing framework powered by Vite.',
     url: 'https://vitest.dev/',
-    tags: ['Testing'],
+    category: 'Testing',
   },
   {
     name: 'Storybook',
     description:
       'Storybook is a frontend workshop for building UI components and pages in isolation.',
     url: 'https://storybook.js.org/',
-    tags: ['Testing'],
+    category: 'Testing',
     authorFavorite: true,
   },
   {
@@ -151,94 +206,124 @@ export const tools = [
     description:
       'Chromatic is a visual testing & review tool that scans every possible UI state across browsers to catch visual and functional bugs.',
     url: 'https://www.chromatic.com/',
-    tags: ['Testing'],
+    category: 'Testing',
   },
+];
+
+export const styling = [
   {
     name: 'Tailwind CSS',
     description:
       'A utility-first CSS framework for rapidly building custom user interfaces.',
     url: 'https://tailwindcss.com/',
-    tags: ['CSS'],
+    category: 'Styling',
   },
   {
     name: 'PostCSS',
-    description: 'A tool for transforming CSS with JavaScript plugins',
+    description: 'A tool for transforming CSS with JavaScript plugins.',
     url: 'https://postcss.org/',
-    tags: ['CSS'],
+    category: 'Styling',
   },
+  {
+    name: 'Vanilla Extract',
+    description:
+      "Zero-runtime Stylesheets in TypeScript. Use TypeScript as your preprocessor. Write type‑safe, locally scoped classes, variables and themes, then generate static CSS files at build time.",
+    url: 'https://vanilla-extract.style/',
+    category: 'Styling',
+  },
+  {
+    name: 'Lightning CSS',
+    description:
+      'An extremely fast CSS parser, transformer, and minifier written in Rust. Use it with Parcel, as a standalone library or CLI, or via a plugin with any other tool.',
+    url: 'https://lightningcss.dev/',
+    category: 'Styling',
+  }
+];
+
+export const runtimes = [
   {
     name: 'Deno',
     description:
       'Deno is the open-source JavaScript runtime for the modern web. Built on web standards with zero-config TypeScript, unmatched security, and a complete built-in toolchain, Deno is the easiest, most productive way to JavaScript.',
     url: 'https://deno.land/',
-    tags: ['Runtime'],
+    category: 'Runtime',
   },
   {
     name: 'Node.js',
     description:
       'Node.js is a free, open-source, cross-platform JavaScript runtime environment that lets developers create servers, web apps, command line tools and scripts.',
     url: 'https://nodejs.org/',
-    tags: ['Runtime'],
+    category: 'Runtime',
   },
   {
     name: 'Bun',
     description:
       'Incredibly fast JavaScript runtime, bundler, test runner, and package manager – all in one.',
     url: 'https://bun.sh/',
-    tags: ['Runtime'],
+    category: 'Runtime',
   },
+];
+
+export const packageManagers = [
   {
     name: 'npm',
     description:
       'npm is the default package manager for the JavaScript runtime environment Node.js and is included as a recommended feature in the Node.js installer.',
     url: 'https://www.npmjs.com/',
-    tags: ['Package Manager'],
+    category: 'Package Manager',
   },
   {
     name: 'Yarn',
     description:
       'Yarn is a package manager that doubles down as project manager.',
     url: 'https://yarnpkg.com/',
-    tags: ['Package Manager'],
+    category: 'Package Manager',
   },
   {
     name: 'pnpm',
     description: 'Fast, disk space efficient package manager',
     url: 'https://pnpm.io/',
-    tags: ['Package Manager'],
+    category: 'Package Manager',
   },
   {
     name: 'JSR',
     description:
       'The open-source package registry for modern JavaScript and TypeScript.',
     url: 'https://jsr.io/',
-    tags: ['Package Manager', 'Rust'],
+    category: 'Package Manager',
+    tags: ['Rust'],
   },
+];
+
+export const designSystems = [
   {
     name: 'Radix UI',
     description:
       'An open source component library optimized for fast development, easy maintenance, and accessibility. Just import and go—no configuration required.',
     url: 'https://www.radix-ui.com/',
-    tags: ['Design System', 'Headless UI'],
+    category: 'Design System',
+    tags: ['Headless UI'],
   },
   {
     name: 'shadcn',
     description:
       'Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.',
     url: 'https://ui.shadcn.com/',
-    tags: ['Design System'],
+    category: 'Design System',
   },
   {
     name: 'React Aria',
     description:
       'Craft world-class accessible components with custom styles. Over 40 components with built-in behavior, adaptive interactions, top-tier accessibility, and internationalization out of the box, ready for your styles.',
     url: 'https://react-spectrum.adobe.com/react-aria/',
-    tags: ['Design System', 'Headless UI'],
+    category: 'Design System',
+    tags: ['Headless UI'],
   },
   {
     name: 'Material UI',
-    description: "Material UI is an open-source React component library that implements Google's Material Design. It's comprehensive and can be used in production out of the box.",
+    description:
+      "Material UI is an open-source React component library that implements Google's Material Design. It's comprehensive and can be used in production out of the box.",
     url: 'https://mui.com/material-ui/',
-    tags: ['Design System'],
-  }
+    category: 'Design System',
+  },
 ];
